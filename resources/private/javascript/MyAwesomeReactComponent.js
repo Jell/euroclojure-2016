6a3e9cd2dda570397e87ab83908a692184978c88
Jell <jean-louis@jawaninja.com>
2016-09-04 17:42:04 +0200

    Cleanup code

--- resources/private/javascript/MyAwesomeReactComponent.js
+++ resources/private/javascript/MyAwesomeReactComponent.js
@@ -1,12 +1,10 @@
 import RaisedButton from 'material-ui/RaisedButton';
 
-var MyAwesomeReactComponent = ({titles, callback, others}) =>
+var MyAwesomeReactComponent = ({titles, callback}) =>
   <div>
-    <h1>Yo?!</h1>
-    <pre>{JSON.stringify(others)}</pre>
     {titles.map((title, i) =>
-      <RaisedButton key={i} label={title} onClick={() => callback(title)}/>
+      <RaisedButton key={i}label={title} onClick={() => callback(title)}/>
     )}
-</div>;
+  </div>;
 
 module.exports = MyAwesomeReactComponent;
--- src/euroclojure_cljs/core.cljs
+++ src/euroclojure_cljs/core.cljs
@@ -5,17 +5,19 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:clicked nil}))
 
 (defn hello-world []
-  [:> js/MuiThemeProvider {:muiTheme js/MuiTheme}
-   [:> js/MyAwesomeReactComponent
-    {:titles (map str (range 10))
-     :callback (fn [title] (js/alert (str "clicked JS: " title)))
-     :others {}}]])
+  [:div
+   [:h1 (str "Clicked: " (:clicked @app-state))]
+   [:> js/MuiThemeProvider {:muiTheme js/MuiTheme}
+    [:> js/MyAwesomeReactComponent
+     {:titles (map str (range 10))
+      :callback (fn [title]
+                  (swap! app-state assoc :clicked title))}]]])
 
 (defonce boot
-  (reagent/render-component [hello-world]
+  (reagent/render-component [#'hello-world]
                             (. js/document (getElementById "app"))))
 
 (defn on-js-reload []
