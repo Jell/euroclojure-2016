25841cea67bba6496195451c33e297388e59e852
Jell <jean-louis@jawaninja.com>
2016-09-24 11:04:07 +0200

    Indentation fix

--- resources/private/javascript/Solution3.js
+++ resources/private/javascript/Solution3.js
@@ -1,10 +1,10 @@
 import RaisedButton from 'material-ui/RaisedButton';
 
 var Solution3 = ({children, callback}) =>
-    <div className="slide-padding">
-      <h1>Solution 3: JSX foreign lib</h1>
-      <RaisedButton label="Click Me!" onClick={callback} />
-      {children}
-    </div>;
+  <div className="slide-padding">
+    <h1>Solution 3: JSX foreign lib</h1>
+    <RaisedButton label="Click Me!" onClick={callback} />
+    {children}
+  </div>;
 
 module.exports = Solution3;
