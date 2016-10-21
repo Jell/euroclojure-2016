8832e6b28644521bbbeb0dd5cdb12a7c61370944
Jell <jean-louis@jawaninja.com>
2016-10-21 07:36:00 +0200

    Remove namespaces from code examples

--- src/euroclojure/utils.clj
+++ src/euroclojure/utils.clj
@@ -1,7 +1,13 @@
-(ns euroclojure.utils)
+(ns euroclojure.utils
+  (:require [clojure.string :as str]))
+
+(defn read-src [type path]
+  (cond-> (slurp path)
+    (= "clojure" type)
+    (str/replace-first #"(?s)\(ns euroclojure.+?\(def" "(def")))
 
 (defmacro code-snippet [type path]
   `[:div
     [:pre.path ~path]
     [:> js/Highlight {:className ~type}
-     ~(slurp path)]])
+     ~(read-src type path)]])
